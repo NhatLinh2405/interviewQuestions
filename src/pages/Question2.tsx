648f65646930524a324ed089c4988c038c8e98ac
NhatLinh2405 <nhoklinhpro981@gmail.com>
2023-02-03 00:07:02 +0700

    done

--- src/pages/Question2.tsx
+++ src/pages/Question2.tsx
@@ -1,3 +1,78 @@
+import answer2 from "../assets/answerQuestion2.png";
 export default function Question2() {
-	return <div>Question2</div>;
+	class Stack<T> {
+		private items: T[] = [];
+
+		push(item: T) {
+			this.items.push(item);
+		}
+
+		pop() {
+			return this.items.pop();
+		}
+
+		find(callback: (item: T) => boolean) {
+			return this.items.find(callback);
+		}
+
+		size = (): number => this.items.length;
+
+		isEmpty = (): boolean => !this.items.length;
+	}
+
+	const stack = new Stack<number>();
+	stack.push(1);
+	stack.push(2);
+	stack.push(3);
+
+	return (
+		<div className="wrap">
+			<h2>Question 2</h2>
+			<div className="content">
+				<div className="imgContent-2">
+					<img src={answer2} alt="answer2" />
+				</div>
+				<table className="ml-5">
+					<tbody>
+						<tr>
+							<th>Method</th>
+							<th>Result</th>
+						</tr>
+						<tr>
+							<td>stack.push(1)</td>
+							<td>Stack: [1]</td>
+						</tr>
+						<tr>
+							<td>stack.push(2)</td>
+							<td>Stack: [1, 2]</td>
+						</tr>
+						<tr>
+							<td>stack.push(3)</td>
+							<td>Stack: [1, 2, 3]</td>
+						</tr>
+						<tr>
+							<td>stack.size()</td>
+							<td>3</td>
+						</tr>
+						<tr>
+							<td>stack.pop()</td>
+							<td>3; Stack: [1, 2]</td>
+						</tr>
+						<tr>
+							<td>stack.isEmpty()</td>
+							<td>false</td>
+						</tr>
+						<tr>
+							<td>stack.find(i ={">"} i === 2)</td>
+							<td>2</td>
+						</tr>
+						<tr>
+							<td>stack.find(i ={">"} i === 4)</td>
+							<td>undefined</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	);
 }
--- src/pages/Question3.tsx
+++ src/pages/Question3.tsx
@@ -1,14 +1,23 @@
 import answer3 from "../assets/answerQuestion3.png";
 
 export default function Question3() {
+	function sumBetween(start: number, end: number): number {
+		return ((end - start + 1) * (start + end)) / 2;
+	}
+
+	function calculate(start: number, end: number, double: boolean): number {
+		let result = sumBetween(start, end);
+		return double ? result * 2 : result;
+	}
+
 	return (
 		<div className="wrap">
 			<h2>Question 3</h2>
 			<div className="content">
-				<div className="imgContent">
-					<img src={answer3} alt="" />
+				<div className="imgContent-3">
+					<img src={answer3} alt="answer3" />
 				</div>
-				<div>
+				<div className="ml-5">
 					<p>In my opinion, I will optimize the code as follows.</p>
 					<p>
 						- Give meaningful variable and function names, can describe the function, and must
